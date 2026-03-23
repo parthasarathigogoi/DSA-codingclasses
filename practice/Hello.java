c06da6c11e622db91d3520779b2937ac97d14646
partha sarathi gogoi <parthasarathi9141@email.com>
2026-03-23 09:36:46 +0530

    practice

---
+++ practice/Hello.java
@@ -0,0 +1,9 @@
+
+
+public class Hello {
+    public static void main(String[] args) {
+        String input = "Lambda";
+        System.out.println(input);
+        System.out.println("Hello from Lambda!");
+    }
+}
---
+++ practice/traprain.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class traprain{
+    public static void main(String []args){
+        int []height={2,4,1,4,7,4};
+        int n=height.length;
+        int []leftmax=new int[n];
+        leftmax[0]=height[0];
+        
+
+    }
+}
